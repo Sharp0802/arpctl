9ec304ae283232eb6ceb5e7a57251a04a8a5b61c
Sharp0802 <sharp0802.business@gmail.com>
2023-08-05 14:46:55 +0900

    Feat: Implement wrapper of std::thread
    - inc/xthread.h: Declare wrapper of std::thread
    - src/xthread.cc: Implement wrapper of std::thread

--- inc/xthread.h
+++ inc/xthread.h
@@ -1,7 +1,11 @@
 #ifndef ARPCTL_XTHREAD_H
 #define ARPCTL_XTHREAD_H
 
+#include <utility>
+
 #include "framework.h"
+#include "property.h"
+#include "spinlock.h"
 
 class Worker
 {
@@ -12,6 +16,34 @@ public:
 		RESERVED = 1,
 		OCCUPIED = 2,
 		EXCEPTION = 3
+	};
+
+private:
+	std::mutex _mutex;
+	volatile State _state;
+	volatile bool _token;
+	std::function<void(const volatile bool*)> _fn;
+	std::thread* _worker;
+
+public:
+	explicit Worker(std::function<void(const volatile bool*)> fn);
+	~Worker();
+
+public:
+	void Start();
+
+	void Stop();
+
+	void Join();
+
+	void Abort();
+
+public:
+	readonly<State> State{
+			_get
+			{
+				return _state;
+			}
 	};
 
 public:
--- src/xthread.cc
+++ src/xthread.cc
@@ -1,9 +1,77 @@
 #include "pch.h"
 #include "xthread.h"
+#include "log.h"
+
 
 size_t Worker::ID()
 {
 	static std::atomic<size_t> g{ 0 };
 	static thread_local size_t r = g++;
 	return r;
+}
+
+void Worker::Start()
+{
+	std::lock_guard lock(_mutex);
+
+	if (_worker->joinable())
+	{
+		pthread_t pth = _worker->native_handle();
+		pthread_cancel(pth);
+	}
+
+	_token = true;
+	_worker = new std::thread([this](const volatile bool* token)
+	{
+		try
+		{
+			_state = State::OCCUPIED;
+			_fn(token);
+			_state = State::COMPLETED;
+		}
+		catch (const std::exception& e)
+		{
+			LOG(FAIL) << "===== EXCEPTION =====\n"
+					  << e.what();
+			_state = State::EXCEPTION;
+		}
+	}, &_token);
+}
+
+void Worker::Stop()
+{
+	_token = false;
+}
+
+void Worker::Join()
+{
+	if (_worker->joinable())
+	{
+		_worker->join();
+	}
+}
+
+void Worker::Abort()
+{
+	std::lock_guard lock(_mutex);
+
+	_token = false;
+	if (_worker->joinable())
+	{
+		pthread_t pth = _worker->native_handle();
+		pthread_cancel(pth);
+	}
+}
+
+Worker::Worker(std::function<void(volatile const bool*)> fn) :
+		_state(State::RESERVED),
+		_token(false),
+		_fn(std::move(fn)),
+		_worker(nullptr)
+{
+}
+
+Worker::~Worker()
+{
+	Abort();
 }
