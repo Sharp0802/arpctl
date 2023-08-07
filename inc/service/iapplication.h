0b4ba26a0f23ef98fc1cbd7f2e81ca235e31ee17
Sharp0802 <sharp0802.business@gmail.com>
2023-08-07 22:49:47 +0900

    Refactor: Move file into appropriate folder
    - inc/iapplication.h -> inc/service/iapplication.h

---
+++ inc/service/iapplication.h
@@ -0,0 +1,25 @@
+#ifndef ARPCTL_IAPPLICATION_H
+#define ARPCTL_IAPPLICATION_H
+
+#include "framework.h"
+#include "xthread.h"
+
+
+class IApplication
+{
+public:
+	virtual ~IApplication()
+	{
+	}
+
+	virtual void Configure(const std::vector<std::string_view>& argv) = 0;
+
+	virtual void Start() = 0;
+
+	virtual enum Worker::State Join() = 0;
+
+	virtual void Abort() = 0;
+};
+
+
+#endif //ARPCTL_IAPPLICATION_H
