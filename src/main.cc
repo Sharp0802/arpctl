790940c3c5948a1fdd858e4d431e98ea21593c22
Sharp0802 <sharp0802.business@gmail.com>
2023-08-12 23:49:09 +0900

    Refactor: Refactor activator
    - src/main.cc: Refactor activator

--- src/main.cc
+++ src/main.cc
@@ -1,34 +1,43 @@
 #include "pch.h"
 #include "log.h"
-#include "iapplication.h"
+#include "service/iapplication.h"
+#include "service/application.h"
+
+using ApplicationType = Application;
+static_assert(std::is_base_of_v<IApplication, ApplicationType>);
 
 int main(int argc, char* argv[])
 {
-	std::vector<std::string_view> args{ static_cast<size_t>(argc) };
-	for (ssize_t i = 0; i < argc; ++i)
-		args.emplace_back(argv[i]);
+	std::vector<std::string_view> args{ static_cast<size_t>(argc) - 1 };
+	for (ssize_t i = 1; i < argc; ++i)
+		args[i - 1] = argv[i];
 
 	enum Worker::State result;
 
 	try
 	{
-		IApplication app(args);
+		ApplicationType app;
+		if (!app.Configure(args))
+		{
+			LOG(CRIT) << "could not configure application";
+			return 1;
+		}
 		app.Start();
 		result = app.Join();
-		app.Abort();
 	}
 	catch (const std::exception& e)
 	{
-		LOG(CRIT) << "IApplication throws exception.\n"
-					 "IApplication will be terminated immediately\n"
-					 "===== EXCEPTION ====="
-				  << e.what();
+		LOG(CRIT) << "Application throws exception.\n"
+					 "Application will be terminated immediately\n"
+					 "===== EXCEPTION BEGIN =====\n"
+				  << e.what()
+				  << "\n===== EXCEPTION END =====";
 		result = Worker::State::EXCEPTION;
 	}
 
 	if (result == Worker::State::EXCEPTION)
 	{
-		LOG(CRIT) << "IApplication exited with exception";
+		LOG(CRIT) << "Application exited with exception";
 		return -1;
 	}
 
